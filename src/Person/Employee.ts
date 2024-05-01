a65a44c9a498b86214da1df098c29fa763eee968
penh sovannarith <sovannarith.penh@student.passerellesnumeriques.org>
2024-05-01 07:29:24 +0700

    airlineManager

--- src/Person/Employee.ts
+++ src/Person/Employee.ts
@@ -1,10 +1,11 @@
 import { Person } from "./Person";
 import { DepartmantType } from "../Enum/DepartmentType";
 
+
 export class Employee extends Person {
     private employeeID: number;
-    private salary: number; 
+    protected salary: number; 
     private position: string;
     private hireDate: Date;
-
+    
 }
--- src/Person/Person.ts
+++ src/Person/Person.ts
@@ -1,24 +1,11 @@
-import { Gender } from "../Enum/gender";
 
 export abstract class Person {
-    protected firstName: string;
-    protected lastName: string;
-    protected gender: Gender;
-    protected dateOfBirth: Date;
-    protected email: string;
-    protected phoneNumber: string;
-    protected Nationally: string; 
-    protected address: string;    
-    protected passportNumber: string;
-
-    constructor(firstName: string, lastName: string,gender:Gender,dateOfBirth:string, email: string, phoneNumber: string, nationality: string, address: string, passportNumber: string){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.Nationally = nationality;
-        this.address = address;
-        this.passportNumber = passportNumber;
-        this.gender = gender;
-    }
+    constructor(
+        protected firstName: string,
+        protected lastName: string,
+        protected email: string,
+        protected phone: string,
+        protected employee_id: string,
+        protected salary: number,
+    ) { }
 }
